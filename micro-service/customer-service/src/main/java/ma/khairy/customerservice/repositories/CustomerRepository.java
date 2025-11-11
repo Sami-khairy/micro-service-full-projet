5b1719e2fff74e3ce4d3fe3db5d92936332fff31
Sami-Khairy <samikhairy2003@gmail.com>
2025-11-11 11:29:30 +0100

    first & last cmt

---
+++ micro-service/customer-service/src/main/java/ma/khairy/customerservice/repositories/CustomerRepository.java
@@ -0,0 +1,9 @@
+package ma.khairy.customerservice.repositories;
+
+import ma.khairy.customerservice.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
---
+++ micro-service/inventory-service/src/main/java/ma/khairy/inventoryservice/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package ma.khairy.inventoryservice.repositories;
+
+import ma.khairy.inventoryservice.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ProductRepository extends JpaRepository<Product,String> {
+}
